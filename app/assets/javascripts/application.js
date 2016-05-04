5b8817c9d6c0a0645058a144bae87649848cb172
Victor Piousbox <piousbox@gmail.com>
2016-05-03 20:33:02 -0700

    some more assets are in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 //
-//= require jquery-1.7.1
+//= require jquery/jquery-1.7.1
 //= require jquery_ujs
-//= require jquery-ui.min
+//= require jquery/jquery-ui.min
 //
 //= require ./utils
 //= require_self
