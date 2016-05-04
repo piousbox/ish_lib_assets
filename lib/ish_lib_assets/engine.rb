18fdcf31939494a78d3d22dfe3895f4d1e79e16b
Victor Piousbox <piousbox@gmail.com>
2016-05-03 22:24:20 -0700

    pagination ok

--- lib/ish_lib_assets/engine.rb
+++ lib/ish_lib_assets/engine.rb
@@ -1,5 +1,9 @@
+
 module IshLibAssets
   class Engine < ::Rails::Engine
     isolate_namespace IshLibAssets
+    config.to_prepare do
+      # ApplicationController.helper(ActionView::Helpers::ApplicationHelper)
+    end
   end
 end
